f1d3d8f2c3bad1131fa716b47249695dc6e83690
iankooo <ianko_radoslav@yahoo.com>
2021-05-06 22:00:20 +0300

    initial commit

---
+++ app/src/main/java/com/schipala_ianko_radoslav/newsreader/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.schipala_ianko_radoslav.newsreader.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
